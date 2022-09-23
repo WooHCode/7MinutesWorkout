fdf724e7805f6998fd2ce41bfb6482f225a86045
WooHCode <gusdn526@naver.com>
2022-09-24 01:43:21 +0900

    Initial commit

---
+++ app/src/main/java/kr/tutorials/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,32 @@
+package kr.tutorials.a7minutesworkout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.Toast
+import kr.tutorials.a7minutesworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        /* findViewById 방식*/
+        //val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        binding?.flStart?.setOnClickListener()
+        {
+           val intent = Intent(this,ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+    }
+}
